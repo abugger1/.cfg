425d068886fee1de9077fd7dbe8c2999b900fe73
abugger1 <abugger1@github.com>
2022-12-07 16:05:14 -0500

    starting over

---
+++ .local/share/plasma/look-and-feel/Moe-Dark/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,43 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "ItemGeometries-1920x1080": "",
+                    "ItemGeometriesHorizontal": "",
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "815",
+                    "DialogWidth": "1198"
+                },
+                "/Configuration": {
+                    "PreloadWeight": "0"
+                },
+                "/General": {
+                    "ToolBoxButtonState": "topcenter",
+                    "ToolBoxButtonX": "465",
+                    "ToolBoxButtonY": "26"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "Image": "/media/Datos/wallpapers/moedark.png",
+                    "SlidePaths": "/home/jorge/.local/share/wallpapers,/usr/share/wallpapers"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
